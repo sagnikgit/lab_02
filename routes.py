54cee7aebd600a1b571d02f9459950286a8506c7
Sagnik Chakraborty <notsupsoup@sagniks-air.attlocal.net>
2019-07-08 17:25:02 -0500

    Third commit

--- routes.py
+++ routes.py
@@ -1,10 +1,15 @@
 from flask import Flask, render_template
+import pandas as pandas
+import sklearn
+from sklearn.externals import joblib
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-	return render_template("index.html")
+	model = joblib.load('regressor.pkl')
+	prediction = int(model.predict([[5, 100, 5000, 52]]).round(1))
+	return render_template("index.html", prediction=prediction)
 
 if __name__ == "__main__":
 	app.run(debug=True)
